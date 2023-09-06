864e72b3a00a466d450d9c9a76fb11582d8b6aa5
shifty-d <ffoxy2870@gmail.com>
2023-09-06 21:12:57 +0300

    initial bringup

---
+++ full_TB-7304I.mk
@@ -0,0 +1,9 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+PRODUCT_DEVICE := TB-7304I
+PRODUCT_NAME := omni_TB-7304I
+PRODUCT_BRAND := Lenovo
+PRODUCT_MODEL := TB-7304I
+PRODUCT_MANUFACTURER := Lenovo
+PRODUCT_BOARD := mt6737m
---
+++ omni_TB7304I.mk
@@ -0,0 +1,5 @@
+$(call inherit-product, vendor/omni/config/common.mk)
+
+$(call inherit-product, device/Lenovo/TB-7304I/full_TB-7304I.mk)
+
+PRODUCT_NAME := omni_TB7304I
